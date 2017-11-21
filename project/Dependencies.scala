f5a68e69a516524db0a85d1eba63468f3ead81f0
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-11-21 07:41:44 +0000

    Resolve "Implement typescript (io-ts) generator"

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,10 +12,12 @@ object Dependencies {
     val pprint = "0.5.3"
     val kindProjector = "0.9.4"
     val paiges = "0.2.0"
+    val scalacheck = "1.13.5"
   }
 
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % Versions.scalaXml
   val specs2 = "org.specs2" %% "specs2-core" % Versions.specs2
+  val specs2ScalaCheck = "org.specs2" %% "specs2-scalacheck" % Versions.specs2
   val cats = "org.typelevel" %% "cats-core" % Versions.cats
   val shapeless = "com.chuusai" %% "shapeless" % Versions.shapeless
   val monocleCore = "com.github.julien-truffaut" %% "monocle-core" % Versions.monocle
@@ -24,6 +26,7 @@ object Dependencies {
   val kindProjector = "org.spire-math" %% "kind-projector" % Versions.kindProjector
   val paigesCore = "org.typelevel" %% "paiges-core" % Versions.paiges
   val paigesCats = "org.typelevel" %% "paiges-cats" % Versions.paiges
+  val scalacheck = "org.scalacheck" %% "scalacheck" % Versions.scalacheck
 
   val xsd = Seq(
     scalaXml,
@@ -44,6 +47,8 @@ object Dependencies {
     paigesCats
   ) ++ Seq(
     specs2,
-    pprint
+    pprint,
+    scalacheck,
+    specs2ScalaCheck
   ).map(_ % "test")
 }
