be2dc30ee8a21af69c235c0934bca25c0771b3fe
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-12-21 10:48:01 +0700

    Added fes-2.0 schema to sbt plugin test, fixed the test

--- sbt-iots/src/sbt-test/iots/simple/src/index.ts
+++ sbt-iots/src/sbt-test/iots/simple/src/index.ts
@@ -1,5 +1,6 @@
 import * as t from "io-ts"
 import * as P from "../src_generated/polaris-1.0"
+import * as F from "../src_generated/fes-2.0"
 
 let x: P.ScanInfoT = {
   mimeType: "application/octet-stream",
@@ -7,3 +8,9 @@ let x: P.ScanInfoT = {
 }
 
 console.log(t.validate(x, P.ScanInfoTType))
+
+let y: F.DateIntervalT = {
+  from: new Date()
+}
+
+console.log(t.validate(y, F.DateIntervalTType))
